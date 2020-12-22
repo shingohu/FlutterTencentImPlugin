cc100f7e101326a7fa846244214ed04c938c4308
shingohu <shingohu@163.com>
2020-12-22 18:58:12 +0800

    tim更新

--- lib/tim.dart
+++ lib/tim.dart
@@ -4,7 +4,10 @@ export 'package:tencent_im_plugin/entity/conversation_entity.dart';
 
 export 'package:tencent_im_plugin/utils/time_util.dart';
 export 'package:tencent_im_plugin/entity/user_entity.dart';
-
-
+export 'package:tencent_im_plugin/enums/message_elem_type_enum.dart';
+export 'package:tencent_im_plugin/enums/message_status_enum.dart';
 export 'package:tencent_im_plugin/entity/find_message_entity.dart';
 export 'package:tencent_im_plugin/entity/message_entity.dart';
+
+export 'package:tencent_im_plugin/message_node/text_message_node.dart';
+export 'package:tencent_im_plugin/message_node/image_message_node.dart';
--- lib/utils/time_util.dart
+++ lib/utils/time_util.dart
@@ -74,13 +74,13 @@ String timestampToChatTime(int timestamp) {
 
   if ((compareTime.millisecondsSinceEpoch -inputTime.millisecondsSinceEpoch).abs()<1000*60*60*24) {
     ///昨天
-    return "昨天";
+    return "昨天 " + DateFormat("HH:mm").format(inputTime) ;
   } else {
     compareTime = DateTime(year, DateTime.january, 1);
     if (compareTime.isAfter(inputTime)) {
-      return DateFormat('yyyy年MM月dd日').format(inputTime);
+      return DateFormat('yyyy年MM月dd日 HH:mm').format(inputTime);
     } else {
-      return DateFormat('MM月dd日').format(inputTime);
+      return DateFormat('MM月dd日 HH:mm').format(inputTime);
 
     }
   }
