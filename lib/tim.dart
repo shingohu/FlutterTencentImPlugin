6c0d8164c725cb7e60a6b80acaa589516f426b07
shingohu <shingohu@163.com>
2020-12-28 09:19:03 +0800

    no message

--- lib/tim.dart
+++ lib/tim.dart
@@ -5,11 +5,11 @@ export 'package:tencent_im_plugin/entity/user_entity.dart';
 export 'package:tencent_im_plugin/enums/image_type_enum.dart';
 export 'package:tencent_im_plugin/enums/message_elem_type_enum.dart';
 export 'package:tencent_im_plugin/enums/message_status_enum.dart';
-export 'package:tencent_im_plugin/message_node/message_node.dart';
 export 'package:tencent_im_plugin/enums/tencent_im_listener_type_enum.dart';
 export 'package:tencent_im_plugin/message_node/image_message_node.dart';
+export 'package:tencent_im_plugin/message_node/message_node.dart';
+export 'package:tencent_im_plugin/message_node/sound_message_node.dart';
 export 'package:tencent_im_plugin/message_node/text_message_node.dart';
+export 'package:tencent_im_plugin/message_node/video_message_node.dart';
 export 'package:tencent_im_plugin/tencent_im_plugin.dart';
 export 'package:tencent_im_plugin/utils/time_util.dart';
-export 'package:tencent_im_plugin/message_node/video_message_node.dart';
-
